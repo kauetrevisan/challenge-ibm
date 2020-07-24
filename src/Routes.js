3ad1fbf31733699cf72f2af0c98d314f67a7362f
kauetrevisan <kauetrevisann@gmail.com>
2020-07-24 01:21:39 -0300

    Create a /dashboard route

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // Pages
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 const routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
